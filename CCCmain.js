8e4cc54deed9f758da5cae6f317a7c49d297e41f
Masha Sakharava <40704018+mashasakharava@users.noreply.github.com>
2018-07-24 10:56:21 -0700

    Added web pages and images

---
+++ CCCmain.js
@@ -0,0 +1,80 @@
+schools ={
+  "UCSB":{"Location": {lat: 34.4140, lng: -119.8489},"Image":"collegeimages/ucsb.jpg", "Acceptance Rate": 33, "SAT": "1220-1460", "Avg GPA": 4.02, "Student pop": 22186, "Tuition($,per year)": 14409, "Top majors": "Social Sciences, Biological and Biomedical Sciences, Psychology","Website":"https://www.ucsb.edu"},
+  "UCSC":{"Location": {lat: 36.9916, lng: -122.0583},"Image":"collegeimages/ucsc.jpg", "Acceptance Rate": 51,  "SAT": "1150-1370","Avg GPA": 3.76, "Student pop": 16962, "Tuition($,per year)": 13515, "Top majors": "Psychology, Economics, Cellular/Molecular Biology","Website":"https://www.ucsc.edu"},
+  "UC Riverside":{"Location": {lat: 33.9737, lng: -117.3281},"Image":"collegeimages/ucriverside.jpg","Acceptance Rate": 66,  "SAT": "1090-1300","Avg GPA": 3.69, "Student pop": 20069, "Tuition($,per year)": 13917, "Top majors": "Social Sciences, Biological/Biomedical Sciences,Marketing","Website":"https://www.ucr.edu"},
+  "UC Irvine":{"Location": {lat: 33.6405, lng: -117.8443},"Image":"collegeimages/ucirvine.png", "Acceptance Rate": 40,  "SAT": "1170-1410", "Avg GPA": 3.94, "Student pop": 29307, "Tuition($,per year)": 15516,"Top majors": "Biology, Social Psychology, Public Health","Website":"https://www.uci.edu"},
+  "UCSD":{"Location": {lat: 32.8801, lng: -117.2340},"Image":"collegeimages/ucsd.jpg", "Acceptance Rate": 35,  "SAT": "1250-1450", "Avg GPA": 4, "Student pop": 28127, "Tuition($,per year)": 14028, "Top majors": "Biology, Computer Engineering, Economics","Website":"https://www.ucsd.edu"},
+  "UCLA":{"Location": {lat: 34.0689, lng: -118.4452},"Image":"collegeimages/ucla.jpg", "Acceptance Rate": 17,  "SAT": "1250-1500", "Avg GPA": 4.31, "Student pop": 31002, "Tuition($,per year)": 13749, "Top majors": "Biology, Business Economics, Political Science","Website":"https://www.ucla.edu"},
+  "UC Berkeley":{"Location": {lat: 37.8719, lng: -122.2585},"Image":"collegeimages/ucberkeley.jpg", "Acceptance Rate": 15,  "SAT": "1330-1540", "Avg GPA": 3.87, "Student pop": 29047, "Tuition($,per year)": 14170,"Top majors": "Engineering, Mathematics & Statistics, Biological/Biomedical Sciences","Website":"https://www.berkeley.edu"},
+  "UC Davis":{"Location": {lat: 38.5382, lng: -121.7617},"Image":"collegeimages/ucdavis.jpg","Acceptance Rate": 38,  "SAT": "1160-1410", "Avg GPA": 4, "Student pop": 30145, "Tuition($,per year)": 14382,"Top majors": "Psychology, Physiology, Biological Sciences","Website":"https://www.ucdavis.edu"},
+  "UC Merced":{"Location": {lat: 37.3642, lng: -120.4255},"Image":"collegeimages/ucmerced.jpg", "Acceptance Rate": 61,  "SAT": "990-1210", "Avg GPA": 3.56, "Student pop": 7375, "Tuition($,per year)": 13598, "Top majors": "Psychology, Engineering, Social Sciences","Website":"https://www.ucmerced.edu"},
+  "Cal Poly SLO":{"Location": {lat: 35.3050, lng: -120.6625},"Image":"collegeimages/calpolyslo.jpg", "Acceptance Rate": 29,  "SAT": "1220-1420", "Avg GPA": 3.92, "Student pop": 20426,"Tuition($,per year)": 9432, "Top majors": "Engineering, Agriculture, Business","Website":"https://www.calpoly.edu"},
+  "CSU Long Beach":{"Location": {lat: 33.7838, lng: -118.1141},"Image":"collegeimages/csulongbeach.jpg", "Acceptance Rate": 34,  "SAT": "1010-1240", "Avg GPA": 3.54, "Student pop": 31324, "Tuition($,per year)": 6798, "Top majors": "Business, Management, Marketing","Website":"https://www.csulb.edu"},
+  "CSU Fullerton":{"Location": {lat: 33.8829, lng: -117.8869},"Image":"collegeimages/csufullerton.jpg", "Acceptance Rate": 44,  "SAT": "1010-1190", "Avg GPA": 3.53, "Student pop": 34920, "Tuition($,per year)": 6830, "Top majors": "Journalism, Marketing, Communication","Website":"http://www.fullerton.edu"},
+  "San Diego State":{"Location": {lat: 32.7157, lng: -117.1611},"Image":"collegeimages/sandiegostate.jpg", "Acceptance Rate": 34,  "SAT": "1090-1300", "Avg GPA": 3.69, "Student pop": 30165, "Tuition($,per year)": 7460, "Top majors": "Management, Marketing, Social Sciences","Website":"http://www.sdsu.edu"},
+  "SFSU":{"Location": {lat: 37.7219, lng: -122.4782},"Image":"collegeimages/sfsu.jpg","Acceptance Rate": 68,  "SAT": "950-1150", "Avg GPA": 3.23, "Student pop": 26615, "Tuition($,per year)": 7254, "Top majors": "Performing Arts, Biological/Biomeidcal Sciences, Communication","Website":"http://www.sfsu.edu"},
+  "SJSU":{"Location": {lat: 37.3352, lng: -121.8811},"Image":"collegeimages/sjsu.jpg","Acceptance Rate": 55,  "SAT": "1010-1250", "Avg GPA": 3.4, "Student pop": 27778,"Tuition($,per year)": 7721, "Top majors": "Engineering, Performing Arts, Management","Website":"http://www.sjsu.edu"},
+  "Chico State":{"Location": {lat: 39.7285, lng: -121.8375},"Image":"collegeimages/chicostate.jpg", "Acceptance Rate": 65,  "SAT": "970-1180", "Avg GPA": 3.27, "Student pop": 16670, "Tuition($,per year)": 7348, "Top majors": "Management, Biological Science, Social Sciences","Website":"http://www.csuchico.edu"},
+  "Sonoma State":{"Location": {lat: 38.3386, lng: -122.6748},"Image":"collegeimages/sonomastate.jpg", "Acceptance Rate": 77,  "SAT": "980-1180", "Avg GPA": 3.24, "Student pop": 8631, "Tuition($,per year)": 7724, "Top majors": "Business Administration and Management, Sociology, Liberal Arts","Website":"https://www.sonoma.edu"},
+  "Cal Poly Pomona":{"Location": {lat: 34.0576, lng: -117.8207},"Image":"collegeimages/calpolypomona.jpg", "Acceptance Rate": 39,  "SAT": "1010-1260", "Avg GPA": 3.49, "Student pop": 24319, "Tuition($,per year)": 7027, "Top majors": "Business Administration and Management, Hopsitality, Civil Engineering","Website":"http://www.cpp.edu"},
+  "CSU Los Angeles":{"Location": {lat: 34.0668, lng: -118.1671},"Image":"collegeimages/csula.jpg", "Acceptance Rate": 61,  "SAT": "870-1070", "Avg GPA": 3.21, "Student pop": 24629, "Tuition($,per year)": 6632, "Top majors": "Management, Psychology, Health Professions","Website":"http://www.calstatela.edu"},
+  "CSU Northridge":{"Location": {lat: 34.2410, lng: -118.5277},"Image":"collegeimages/csunorthridge.jpg", "Acceptance Rate": 46,  "SAT": "880-1110", "Avg GPA": 3.18, "Student pop": 35609, "Tuition($,per year)": 6569, "Top majors": "Engineering, Computer Science, Pan African Studies","Website":"https://www.csun.edu"},
+  "CalTech":{"Location": {lat: 34.1377, lng: -118.1253},"Image":"collegeimages/caltech.jpg", "Acceptance Rate": 9,  "SAT": "1530-1600", "Avg GPA": 4.24, "Student pop": 2238, "Tuition($,per year)": 49908, "Top majors": "Engineering, Computer Information Sciences and Support Services, Mathematics and Statistics","Website":"http://www.caltech.edu"},
+  "USC":{"Location": {lat: 34.0224, lng: -118.2851},"Image":"collegeimages/usc.jpg", "Acceptance Rate": 18,  "SAT": "1360-1540","Avg GPA": 3.73, "Student pop": 19287, "Tuition($,per year)": 54323,"Top majors": "Business, Visual and Performing Arts, Engineering","Website":"https://www.usc.edu"},
+  "Stanford":{"Location": {lat: 37.4275, lng: -122.1697},"Image":"collegeimages/stanford.jpg", "Acceptance Rate": 5,  "SAT": "1450-1590", "Avg GPA": 3.95, "Student pop": 7062,"Tuition($,per year)": 49617, "Top majors": "Computer and Information Sciences, Multi/Interdiscplinary Sciences, Biological/Biomedical Sciences","Website":"https://www.stanford.edu"},
+  "Chapman":{"Location": {lat: 33.7933, lng: -117.8514},"Image":"collegeimages/chapman.jpg", "Acceptance Rate": 48,  "SAT": "1240-1450", "Avg GPA": 3.69,"Student pop": 7020, "Tuition($,per year)": 52724, "Top majors": "Business Administration and Management, Cinematography, Speech Communication","Website":"https://www.chapman.edu"},
+  "Pepperdine":{"Location": {lat: 34.0414, lng: -118.7096},"Image":"collegeimages/pepperdine.jpg", "Acceptance Rate": 38,  "SAT": "1190-1390", "Avg GPA": 3.59, "Student pop": 3604, "Tuition($,per year)": 51992, "Top majors": "Management, Marketing, Multi/Interdiscplinary Studies","Website":"https://www.pepperdine.edu"},
+  "USF":{"Location": {lat: 37.7765, lng: -122.4506},"Image":"collegeimages/usf.jpg", "Acceptance Rate": 64,  "SAT": "1150-1340", "Avg GPA": 3.63, "Student pop": 6847, "Tuition($,per year)": 46250, "Top majors": "Business Administration and Management, Registered Nursing, Psychology","Website":"https://www.usfca.edu"},
+  "LMU":{"Location": {lat: 33.9702, lng: -118.4166},"Image":"collegeimages/lmu.jpg", "Acceptance Rate": 50,  "SAT": "1180-1360", "Avg GPA": 3.75,"Student pop": 6391, "Tuition($,per year)": 46386, "Top majors": "Management, Visual and Peforming Arts, Communication","Website":"https://www.lmu.edu"},
+  "Claremont McKenna":{"Location": {lat: 34.1018, lng: -117.7079},"Image":"collegeimages/claremonmckenna.jpg", "Acceptance Rate": 11,  "SAT": "1430-1550", "Avg GPA": 4.05, "Student pop": 1338, "Tuition($,per year)": 54405, "Top majors": "Economics, Political Science and Government, Accounting","Website":"https://www.cmc.edu"},
+  "Santa Clara University":{"Location": {lat: 37.3496, lng: -121.9390},"Image":"collegeimages/scu.jpg", "Acceptance Rate": 49,  "SAT": "1280-1480", "Avg GPA": 3.67, "Student pop": 5499, "Tuition($,per year)": 49858, "Top majors": "Marketing, Business, Engineering","Website":"https://www.scu.edu"},
+  "St.Mary's College":{"Location": {lat: 37.8413, lng: -122.1101},"Image":"collegeimages/stmaryscollege.jpeg", "Acceptance Rate": 69,  "SAT": "1080-1270" , "Avg GPA": 3.61, "Student pop": 2777, "Tuition($,per year)": 47280,"Top majors": "Business, General Studies and Humanities, Liberal Arts and Sciences","Website":"https://www.stmarys-ca.edu"},
+}
+
+var schoolBtns = document.querySelectorAll('li')
+
+function myMap() {
+  var mapProp= {
+      center:new google.maps.LatLng(36.7783,-119.4179),
+      zoom:6.2,
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+  var schoolnames = Object.keys(schools)
+  for (var i=0;i<schoolnames.length;i++) {
+    console.log(schools[schoolnames[i]]['Location'])
+    var myLatLng = schools[schoolnames[i]]['Location']
+    let marker = new google.maps.Marker({
+              position: myLatLng,
+              map: map,
+            });
+    let infowindow = new google.maps.InfoWindow({
+              content: `<font size=+2>School:<b>${schoolnames[i]}</b></font> <br><img src="${schools[schoolnames[i]]['Image']}" width=380 height=190><br><b>Location</b>: ${schools[schoolnames[i]]['Location']["lat"]}, ${schools[schoolnames[i]]['Location']["lng"]} <br> <b>Acceptance Rate(%)</b>: ${schools[schoolnames[i]]['Acceptance Rate']}<br><b> SAT</b>: ${schools[schoolnames[i]]['SAT']}<br><b> Average GPA:</b>
+               ${schools[schoolnames[i]]['Avg GPA']}<br><b>Student Population</b>: ${schools[schoolnames[i]]['Student pop']}<br><b>Tuition($,per year)</b>: ${schools[schoolnames[i]]['Tuition($,per year)']}<br> <b>Top Majors</b>: ${schools[schoolnames[i]]['Top majors']}<br> <b>Website</b>:<a href="${schools[schoolnames[i]]['Website']}">Link to school's website</a> `,
+              maxWidth: 400,
+            });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+        for (var j=0;j<schoolBtns.length;j++) {
+          if (schoolBtns[j].innerText == schoolnames[i] ){
+            schoolBtns[j].addEventListener('click', function() {
+                infowindow.open(map, marker);
+              });
+
+
+          }
+        }
+
+    //document.querySelector('li')
+    //let schoolButton =
+      //find li with matching school name
+      // addListener to that li
+  }
+}
+
+
+//For each li in the document
+// add function which takes the text from the li
+// uses it to find the college in the json
+// use the json to center, zoom, and display the pin data on the map
